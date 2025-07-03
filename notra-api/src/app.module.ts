3b4619654dc5340f4904726bf7eef560b557ae63
leviutima <levi.utima@gmail.com>
2025-07-03 13:56:45 -0300

    feat: create auth module

--- notra-api/src/app.module.ts
+++ notra-api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { UserModule } from './users/user.modules';
+import { UserModule } from './users/user.module';
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
   controllers: [],
   providers: [PrismaService],
 })
