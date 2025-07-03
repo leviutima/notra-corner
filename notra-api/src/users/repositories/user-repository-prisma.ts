97e387f92ef2e7b4ae3067178707180b6dbd2b26
leviutima <levi.utima@gmail.com>
2025-07-03 02:46:25 -0300

    feat: connect database

--- notra-api/src/users/repositories/user-repository-prisma.ts
+++ notra-api/src/users/repositories/user-repository-prisma.ts
@@ -1,12 +1,21 @@
 import { Injectable } from "@nestjs/common";
 import { UserRepository } from "./user-repository";
 import { User } from "../domain/user.entity";
+import { PrismaService } from "src/database/prisma.service";
 
 @Injectable()
 export class PrismaUserRepository implements UserRepository {
+    constructor(
+        private prisma: PrismaService
+    ) {}
+
 
     createUser(user: User): Promise<User> {
-        throw new Error("Method not implemented.");
+        return this.prisma.user.create({
+            data: {
+                
+            }
+        })
     }
     findByEmail(email: string): Promise<User | null> {
         throw new Error("Method not implemented.");
