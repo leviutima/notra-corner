4648c062cfcf163c45b81ae94db2251646c67785
leviutima <levi.utima@gmail.com>
2025-07-03 13:49:42 -0300

    feat: implement notation controller and post in controller auth

--- notra-api/src/auth/auth.controller.ts
+++ notra-api/src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Res } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { Response } from 'express';
 
+@Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post("login")
   async login(
     @Body() dto: LoginDto,
     @Res({ passthrough: true }) res: Response,
