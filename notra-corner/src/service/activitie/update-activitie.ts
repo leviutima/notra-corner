ed509028ca2af41e1f184ab9dc39210a2a37fb93
leviutima <levi.utima@gmail.com>
2025-07-22 20:22:14 -0300

    feat: create req update activitie in front-end

---
+++ notra-corner/src/service/activitie/update-activitie.ts
@@ -0,0 +1,19 @@
+import { api } from "../api";
+
+interface updateActivitieProps {
+  title: string;
+  description: string;
+}
+
+export const updateActivitie = async (
+  activitieId: string,
+  data: updateActivitieProps
+) => {
+  try {
+    const res = await api.put(`/activitie/${activitieId}`, data);
+    return res.data;
+  } catch (err: any) {
+    console.log(err);
+    throw Error;
+  }
+};
