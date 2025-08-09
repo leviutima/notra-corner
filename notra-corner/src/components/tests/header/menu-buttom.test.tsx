1c98c91c3d2d7acd240a6bb7643d095caa6ea59e
leviutima <levi.utima@gmail.com>
2025-08-09 09:02:52 -0300

    test: test button sign-in

---
+++ notra-corner/src/components/tests/header/menu-buttom.test.tsx
@@ -0,0 +1,15 @@
+import { MenuButtons } from "@/components/header/header-LP/menu-buttons"
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom";
+
+jest.mock('next/link',() => {
+  return ({children, href}: any) => <a href={href}>{children}</a>
+})
+
+describe('Menu buttons header',() => {
+  it('Should click redirect sign-in page', () => {
+    render(<MenuButtons />)
+    const linkElement = screen.getByRole("link", {name:/login/i})
+    expect(linkElement).toHaveAttribute('href', "/auth/sign-in")
+  })
+})
